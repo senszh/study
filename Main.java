714666609555656e6b84abc65b1d5c365059d603
senszh <senazahterogluu@gmail.com>
2022-10-21 18:43:16 +0300

    dosyalar eklendi

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    static boolean isPalindrom(int number){
+        int temp=number,lastNumber,reverseNumber=0;
+        while (temp!=0){
+            lastNumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            temp/=10;
+        }
+        if(number==reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(828));
+    }
+}
